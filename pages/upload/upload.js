ea1bc771fc7afbc9e05dba6ab2efc7ce7741428f
Rachel <zxm1357129218@163.com>
2019-09-04 15:17:12 +0800

    校园打印线上版本

---
+++ pages/upload/upload.js
@@ -0,0 +1,75 @@
+// pages/upload/upload.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**isImg
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    let info = wx.getStorageSync('isImg')
+    console.log(info, '88888888')
+  },
+  print(e) {
+    console.log(e,'44')
+    let isImg = e.currentTarget.dataset.isimg
+    wx.navigateTo({
+      url: '/pages/buycar/buycar?isImg=' + isImg,
+
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
